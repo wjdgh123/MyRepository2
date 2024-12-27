42d193ce557561b2de4b72a8a5a9a1e14a31e122
devB <21kjh@BOOK-GDUMOSJ3R0>
2024-12-27 15:20:51 +0900

    Initial version

---
+++ MyGitTest/src/mygit/B.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("This is B");
+
+	}
+
+}
---
+++ MyGitTest/src/mygit/C.java
@@ -0,0 +1,10 @@
+package mygit;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("This is C");
+
+	}
+
+}
